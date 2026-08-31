270d569ea0f813009d4b05f49ba88b2385ab28ae
monourar <monourar@gmail.com>
2026-08-31 19:43:28 +0100

    fix: remove maintenance interception from layout and disable default maintenanceMode

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -5,18 +5,12 @@ import { SiteFooter } from "@/components/layout/site-footer";
 import { MobileBottomNav } from "@/components/layout/mobile-bottom-nav";
 import { EmergencyFab } from "@/components/interactive/emergency-fab";
 import { WelcomeDialog } from "@/components/interactive/welcome-dialog";
-import { MaintenanceScreen } from "@/components/shared/maintenance-screen";
-import { siteConfig } from "@/config/site";
 import { getDictionary } from "@/i18n/dictionaries";
 import { getLocale } from "@/i18n/server";
 
 export default async function SiteLayout({ children }: { children: React.ReactNode }) {
   const locale = await getLocale();
   const t = await getDictionary(locale);
-
-  if (siteConfig.maintenanceMode) {
-    return <MaintenanceScreen locale={locale} />;
-  }
 
   return (
     <div className="flex min-h-screen flex-col">
